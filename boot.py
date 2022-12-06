9cd9206bffaa1f39a0b22abaec0e07cc5358502a
Алексей Казачина <94910011+alexei0kazachina@users.noreply.github.com>
2022-12-06 18:19:38 +0200

    Add files via upload

--- boot.py
+++ boot.py
@@ -1,3 +1,35 @@
-# This is script that run when device boot up or wake from sleep.
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+import machine
 
+import bme280
 
+import network
+
+try:
+  import usocket as socket
+except:
+  import socket
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'holod'
+password = '77700609'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
--- main.py
+++ main.py
@@ -1,15 +1,69 @@
-from machine import Pin, I2C
-from bmp180 import BMP180
-from time import sleep as delay
+i2c = machine.I2C(scl=machine.Pin(22), sda=machine.Pin(21))
+bme = bme280.BME280(i2c=i2c)
+print(bme.values)
 
-i2c = I2C(scl=Pin(22), sda=Pin(21))
-print('I2C scan:', i2c.scan())
+from machine import Pin
+led = Pin(5, Pin.OUT)
+def web_page(): 
+   if led.value() == 1: 
+      gpio_state="ВКЛ"
+   else: 
+      gpio_state="ВЫКЛ"
+   html = """
+   <!DOCTYPE html>
+   <head>
+   <meta charset="utf-8">
+   <title>Состояние реле компрессор</title
+   <link rel="stylesheet" href="stile.css">
+   <style>
+   .button {
+   border: none;
+   color: white;
+   padding: 15px 32px;
+   text-align: center;
+   text-decoration: none;
+   display: inline-block;
+   font-size: 16px;
+   margin: 4px 2px;
+   cursor: pointer;
+   }
 
-bmp180 = BMP180(i2c)
-bmp180.baseline = 10125
+   .button1 {background-color: #4CAF50;} /* Green */
+   .button2 {background-color: #008CBA;} /* Blue */
+   </style>
+   </head>
+   <body>
+   <div align="center">
+   <h1>Состояние реле K1</h1>
+   <p>GPIO state:<strong>""" + gpio_state + """</strong></p>
+   <p><a href="/?led=on"><button class="button button1">ВКЛ</button></a></p>
+   <p><a href="/?led=off"><button class="button button2">ВЫКЛ</button></a></p>
+   </div> 
+  </body>
+   
+   </html>"""
+   return html
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('',80))
+s.listen(5)
 while True:
-print('T: %.2fC P: %.1fPa A: %dm At: %dm' % (float(bmp180.temperature),
-float(bmp180.pressure),
-int(bmp180.altitude),((float(bmp180.pressure))/133)))
-delay(5)
+  conn, addr = s.accept()
+  print('Got a connection from %s'%str(addr))
+  request = conn.recv(1024)
+  request =str(request)
+  print('Content = %s'% request)
+  led_on = request.find('/?led=on')
+  led_off = request.find('/?led=off')
+  if led_on ==6:
+    print('LED ON')
+    led.value(1)
+  if led_off ==6:
+    print('LED OFF')
+    led.value(0)
+  response =web_page()
+  conn.send('HTTP/1.1 200 OK\n')
+  conn.send('Content-Type: text/html\n')
+  conn.send('Connection: close\n\n')
+  conn.sendall(response)
+  conn.close()
